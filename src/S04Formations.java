59b1fdb235f406a7928aef5115a4a76562fce539
Promo_01 Stagiaires05 <Stagiaires05P@ad.fms-ea.com>
2022-03-17 15:32:02 +0100

    add to cart

--- src/S04Formations.java
+++ src/S04Formations.java
@@ -8,49 +8,53 @@ public class S04Formations {
 	public static void main(String[] args) {
 
 		HashMap<Integer, List<String>> programList = new HashMap<>();
-		HashMap<Integer, List<String>> shoppingCart = new HashMap<>();
+		HashMap<String, Integer> shoppingCart = new HashMap<>();
 		Scanner scan = new Scanner(System.in);
 		int menuChoice = 0;
 		boolean activity = true;
+		int price = 0;
 
 		initData(programList);
 		buildArray(programList);
-		while(activity = true) {
-		System.out.println("Votre panier actuel : ");
-		if (shoppingCart.isEmpty()) {
-			System.out.println(" Votre panier est actuellement vide ");
-		} else {
-			showShoppingCart(shoppingCart);
-		}
-		
-		System.out.println("Que souhaitez vous faire ? ");
-		System.out.println(" 1/ Voir votre panier\n 2/ Ajouter une formation au panier\n 3/ Retirer une formation du panier\n 4/ Supprimer le panier");
-		while (!scan.hasNextInt())
-			scan.next();
-		menuChoice = scan.nextInt();
-		switch (menuChoice) {
-		case 1:
-			showShoppingCart(shoppingCart);
-			break;
-		case 2:
-			addToCart(shoppingCart);
-			break;
-		case 3:
-			removeToCart(shoppingCart);
-			break;
-			
-		case 4:
-			deleteCart(shoppingCart);
-			break;
-		case 0:
-			activity=false;
-			break;
-		}
+		while (activity = true) {
+			System.out.println("Votre panier actuel : ");
+			if (shoppingCart.isEmpty()) {
+				System.out.println(" Votre panier est actuellement vide ");
+			} else {
+				showShoppingCart(shoppingCart, programList);
+			}
+
+			System.out.println("Que souhaitez vous faire ? ");
+			System.out.println(
+					" 1/ Voir votre panier\n 2/ Ajouter une formation au panier\n 3/ Retirer une formation du panier\n 4/ Supprimer le panier");
+
+			while (!scan.hasNextInt())
+				scan.next();
+			menuChoice = scan.nextInt();
+			switch (menuChoice) {
+			case 1:
+				showShoppingCart(shoppingCart, programList);
+				break;
+			case 2:
+				addToCart(shoppingCart, programList);
+				break;
+			case 3:
+				removeToCart(shoppingCart);
+				break;
+
+			case 4:
+				deleteCart(shoppingCart);
+				break;
+			case 0:
+				activity = false;
+				break;
+			}
 		}
 		if (shoppingCart.isEmpty()) {
 			System.out.println("A bientot...");
 		} else {
-			System.out.println("Hop là tu vas où ? On part pas sans payer !!!");}
+			System.out.println("Hop là tu vas où ? On part pas sans payer !!!");
+		}
 	}
 
 	public static void initData(HashMap<Integer, List<String>> programList) {
@@ -84,12 +88,20 @@ public class S04Formations {
 		programList5.add("DotNet Core");
 		programList5.add("5000");
 
-		// build the list
+		// build the list of program
 		programList.put(1, programList1);
 		programList.put(2, programList2);
 		programList.put(3, programList3);
 		programList.put(4, programList4);
 		programList.put(5, programList5);
+		
+		//
+		HashMap<String, Integer> shoppingCart = new HashMap<>();
+		shoppingCart.put("java" ,0);
+		shoppingCart.put("JavaA",0);
+		shoppingCart.put("spring",0);
+		shoppingCart.put("php",0);
+		shoppingCart.put("c#",0);
 
 	}
 
@@ -105,14 +117,48 @@ public class S04Formations {
 		System.out.println("\n----------------------------------------------------------------------------");
 	}
 
-	private static void showShoppingCart(HashMap<Integer, List<String>> shoppingCart) {
+	private static void showShoppingCart(HashMap<String, Integer> shoppingCart, HashMap<Integer, List<String>> programList) {
 		System.out.println(shoppingCart);
+		shoppingCart.entrySet().stream().forEach(System.out::println);
 	}
-	private static void addToCart(HashMap<Integer, List<String>> shoppingCart) {
+
+
+	private static void addToCart(HashMap<String, Integer> shoppingCart,
+			HashMap<Integer, List<String>> programList) {
+		System.out.println("Quelle formation souhaitez vous ajouter ?");
+		System.out.println("[1 - Java] [2 - Java avancé] [3 - Spring] [4 - Php framework] [5 - C#]");
+		int cartChoice = 0;
+		Scanner scanValue = new Scanner(System.in);
+		while (!scanValue.hasNextInt())
+			scanValue.next();
+		cartChoice = scanValue.nextInt();
+		switch (cartChoice) {
+		case 1:
+			shoppingCart.merge("java", 1, Integer::sum);
+			
+			break;
+		case 2:
+			shoppingCart.merge("JavaA", 1, Integer::sum);
+			break;
+		case 3:
+			shoppingCart.merge("spring", 1, Integer::sum);
+			break;
+		case 4:
+			shoppingCart.merge("php", 1, Integer::sum);
+			break;
+		case 5:
+			shoppingCart.merge("c#", 1, Integer::sum);
+			break;
+		}
+		
+		showShoppingCart(shoppingCart, programList);
 	}
-	private static void removeToCart(HashMap<Integer, List<String>> shoppingCart) {
+
+	private static void removeToCart(HashMap<String, Integer> shoppingCart) {
+
 	}
-	private static void deleteCart(HashMap<Integer, List<String>> shoppingCart) {
+
+	private static void deleteCart(HashMap<String, Integer> shoppingCart) {
 	}
-	
+
 }
